df0cb79db43ff36dba26db076b708523711b5916
palomahnhp <hernandezp@madrid.es>
2017-11-16 19:50:00 +0100

    articulos

--- app/controllers/articulos_controller.rb
+++ app/controllers/articulos_controller.rb
@@ -3,28 +3,51 @@ class ArticulosController < ApplicationController
   end
 
   def new
+    @boton = 'Crear'
+    @articulo = Articulo.new
   end
 
   def create
+    @articulo = Articulo.new(articulo_params)
+    if @articulo.save
+      redirect_to @articulo
+    else
+      render :new
+    end
   end
 
   def edit
+    @boton = 'Editar'
+    @articulo = Articulo.find(params[:id])
   end
 
   def update
+    @articulo = Articulo.find(params[:id])
+    if @articulo.update(articulo_params)
+      flash[:notice] = "Articulo modificado correctamente!"
+      redirect_to @articulo
+    else
+      render :edit
+    end
   end
 
   def show
+    @boton = 'Ver'
+    @articulo = Articulo.find(params[:id])
   end
 
   def destroy
+    @articulo = Articulo.find(params[:id])
+    @articulo.destroy
 
+    @boton = 'Eliminar'
+    redirect_to articulos_path, notice: "Articulo eliminado correctamente"
   end
 
   private
 
   def articulo_params
-    params.require(:articulo).permit(:titulo, :body)
+    params.require(:articulo).permit(:titulo, :contenido)
   end
 
 end
--- app/models/articulo.rb
+++ app/models/articulo.rb
@@ -1,3 +1,5 @@
 class Articulo < ApplicationRecord
   belongs_to :autor
+
+  validates :titulo, :contenido, presence: true
 end
